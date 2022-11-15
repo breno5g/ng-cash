69691bee566843e5dc2cf2567b713b70332030c9
Breno Santos <brenosantos145@gmail.com>
2022-11-15 19:34:57 -0300

    fix: docker-compose params and volume

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
+app.get('/', (_req, res) => {
+  return res.status(200).json('ok');
+});
+
+app.listen(process.env.PORT, () => {
   console.log('O pai ta on');
 });
