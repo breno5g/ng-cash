59c86c3fb476d78bcb3313efaa6dfc50359bdc6c
Breno Santos <brenosantos145@gmail.com>
2022-11-15 20:15:46 -0300

    fix: enviroment database url and prisma schema names in user table

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  return res.status(200).json('ok');
-});
-
 app.listen(process.env.PORT, () => {
-  console.log('O pai ta on');
+  console.log('server is on');
 });
