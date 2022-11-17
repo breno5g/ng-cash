83f253dffdf2f8262a093fbd953f376206c20a48
Breno Santos <brenosantos145@gmail.com>
2022-11-16 21:48:16 -0300

    test: fix test logic

---
+++ src/tests/mocks/user.ts
@@ -0,0 +1,12 @@
+const userMock = [
+  {
+    username: 'teste02',
+    password: '$2b$10$OlBXc7Dwt0TyCwLyGYyor.CzI2PIHBGD357CxLo3uyqB2lIxxL/sy',
+    accountId: 1,
+    account: {
+      balance: 100
+    }
+  }
+]
+
+export { userMock }
--- src/tests/services/user.service.test.ts
+++ src/tests/services/user.service.test.ts
@@ -3,26 +3,17 @@ import { describe, it, expect, beforeEach, vitest, afterAll } from 'vitest';
 import { UserAlreadyExistsError } from '../../errors';
 import UserModel from '../../models/User.model';
 
+import { IUser, IUserLogin } from '../../interfaces/IUser';
+import { UserService } from '../../services/User.service';
+
+import { userMock } from "../mocks/user"
+
 describe('User service', () => {
   describe('Create user', () => {
     describe('Success case', () => {
       beforeEach(() => {
         vitest.spyOn(UserModel.prototype, 'create').mockImplementation(async () => 'User created successfuly');
-        vitest.clearAllMocks();
-      });
-
-      it('should be possible create user', async () => {
-        const model = new UserModel(new PrismaClient());
-        const res = await model.create({ username: 'teste', password: '@Teste01' });
-        expect(res).toBe('User created successfuly');
-      });
-    });
-
-    describe('Fail case', () => {
-      beforeEach(() => {
-        vitest.spyOn(UserModel.prototype, 'create').mockImplementation(() => {
-          throw new UserAlreadyExistsError();
-        });
+        vitest.spyOn(UserModel.prototype, 'findOneByUsername').mockImplementation(async () => null)
       });
 
       afterAll(() => {
@@ -30,9 +21,27 @@ describe('User service', () => {
       });
 
       it('should be possible create user', async () => {
+        const service = new UserService(new UserModel(new PrismaClient()));
+        const res = await service.create({ username: 'teste', password: '@Teste01' });
+        expect(res).toBe('User created successfuly');
+      });
+    });
+
+    describe('Fail case', () => {
+      beforeEach(() => {
+        vitest.spyOn(UserModel.prototype, 'findOneByUsername').mockImplementation(async () => ({
+          ...userMock
+        } as unknown as IUser));
+      });
+
+      afterAll(() => {
+        vitest.clearAllMocks();
+      });
+
+      it('should be throw error when user already exists', async () => {
         try {
-          const model = new UserModel(new PrismaClient());
-          await model.create({ username: 'teste', password: '@Teste01' });
+          const service = new UserService(new UserModel(new PrismaClient()));
+          await service.create({ username: 'teste', password: '@Teste01' });
         } catch (error: any) {
           expect(error.message).toBe('User already exists');
           expect(error.status).toBe(409);
