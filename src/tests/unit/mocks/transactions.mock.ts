ce4496ae17e2874a45dc677e27155d9fe1166161
Breno Santos <brenosantos145@gmail.com>
2022-11-19 22:28:31 -0300

    test: get transactions

---
+++ src/tests/unit/mocks/transactions.mock.ts
@@ -0,0 +1,280 @@
+const transactionsMock = [
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:17.306Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:45.373Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:45.928Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:46.477Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:46.975Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:09.472Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:10.017Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:10.420Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  },
+  {
+    "value": 43,
+    "createdAt": "2022-11-19T22:04:18.377Z",
+    "creditedAccount": {
+      "User": {
+        "username": "lucas"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  }
+];
+
+const cashInTransactionsMock = [
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:09.472Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:10.017Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:26:10.420Z",
+    "creditedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    }
+  }
+];
+
+const cashOutTransactionsMock = [
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:17.306Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:45.373Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:45.928Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:46.477Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 100,
+    "createdAt": "2022-11-19T21:25:46.975Z",
+    "creditedAccount": {
+      "User": {
+        "username": "teste"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 43,
+    "createdAt": "2022-11-19T22:04:18.377Z",
+    "creditedAccount": {
+      "User": {
+        "username": "lucas"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  },
+  {
+    "value": 43,
+    "createdAt": "2022-11-19T22:41:08.509Z",
+    "creditedAccount": {
+      "User": {
+        "username": "lucas"
+      }
+    },
+    "debitedAccount": {
+      "User": {
+        "username": "breno5g"
+      }
+    }
+  }
+]
+
+export {
+  transactionsMock,
+  cashInTransactionsMock,
+  cashOutTransactionsMock
+};
--- src/tests/unit/models/transaction.model.test.ts
+++ src/tests/unit/models/transaction.model.test.ts
@@ -1,8 +1,10 @@
 import { User } from '@prisma/client';
 import { describe, it, expect, beforeEach, vitest, afterAll } from 'vitest';
+import { ITransaction } from '../../../interfaces/ITransaction';
 import { TransactionModel } from '../../../models/transaction.model';
 
 import { prismaMock } from '../mocks/prisma.mock';
+import { cashInTransactionsMock, cashOutTransactionsMock, transactionsMock } from '../mocks/transactions.mock';
 import { userMock } from '../mocks/user';
 
 describe('Transaction model', () => {
@@ -71,6 +73,72 @@ describe('Transaction model', () => {
             expect(error.status).toBe(406)
           }
         });
+      });
+    });
+  });
+
+  describe('Get transactions', () => {
+    describe('Success case', () => {
+      beforeEach(() => {
+        prismaMock.transaction.findMany.mockResolvedValue(transactionsMock as any)
+      });
+
+      afterAll(() => {
+        vitest.clearAllMocks();
+      });
+
+      it('should be possible create user', async () => {
+        const model = new TransactionModel(prismaMock);
+        const res = await model.getTransactions(1, {});
+        expect(res).toStrictEqual(transactionsMock);
+      });
+    });
+
+    describe('Success case when pass date', () => {
+      beforeEach(() => {
+        prismaMock.transaction.findMany.mockResolvedValue([] as any)
+      });
+
+      afterAll(() => {
+        vitest.clearAllMocks();
+      });
+
+      it('should be possible create user', async () => {
+        const model = new TransactionModel(prismaMock);
+        const res = await model.getTransactions(1, {date: "2075-12-23"});
+        expect(res).toStrictEqual([]);
+      });
+    });
+
+    describe('Success case when pass cash out', () => {
+      beforeEach(() => {
+        prismaMock.transaction.findMany.mockResolvedValue(cashOutTransactionsMock as any)
+      });
+
+      afterAll(() => {
+        vitest.clearAllMocks();
+      });
+
+      it('should be possible create user', async () => {
+        const model = new TransactionModel(prismaMock);
+        const res = await model.getTransactions(1, {"cash-out": true});
+        expect(res).toStrictEqual(cashOutTransactionsMock);
+      });
+    });
+
+    describe('Success case when pass cash-in', () => {
+      beforeEach(() => {
+        prismaMock.transaction.findMany.mockResolvedValue(cashInTransactionsMock as any)
+      });
+
+      afterAll(() => {
+        vitest.clearAllMocks();
+      });
+
+      it('should be possible create user', async () => {
+        const model = new TransactionModel(prismaMock);
+        const res = await model.getTransactions(1, {"cash-in": true});
+        expect(res).toStrictEqual(cashInTransactionsMock);
       });
     });
   });
