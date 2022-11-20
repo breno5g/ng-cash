d1f2e85488363a47b755205871ec25882d80bdaa
Breno Santos <brenosantos145@gmail.com>
2022-11-19 21:29:19 -0300

    test: login method in controller

--- src/tests/unit/controllers/user.controller.test.ts
+++ src/tests/unit/controllers/user.controller.test.ts
@@ -5,7 +5,7 @@ import {mockError,mockNext,mockRequest,mockResponse} from "../mocks/express"
 import {UserService} from "../../../services/User.service"
 import {UserController} from "../../../controllers/User.controller"
 import UserModel from '../../../models/User.model';
-import { UserAlreadyExistsError } from '../../../errors';
+import { UserAlreadyExistsError, UsernameOrPasswordNotFoundError } from '../../../errors';
 
 describe('User controller', () => {
   describe('Create', () => {
@@ -61,6 +61,66 @@ describe('User controller', () => {
           expect(next).toHaveBeenCalledWith({
             message: "User already exists",
             status: 409
+          })
+      });
+    });
+  });
+
+  describe('Login', () => {
+    describe('Success case', () => {
+      let req = mockRequest();
+      let res = mockResponse();
+      let next = mockNext();
+      const returnMock = {
+        data: { username: "breno5g", account: { balance: 100 } },
+        token: "Fake token"
+      }
+      beforeEach(() => {
+        req.body = {
+          username: "breno5g",
+          password: "@Teste01"
+        }
+        vitest.spyOn(UserService.prototype, 'login').mockImplementation(async () => returnMock);
+      });
+  
+      afterAll(() => {
+        vitest.clearAllMocks()
+      });
+  
+      it('User created successfully', async () => {
+        const controller = new UserController(new UserService({} as UserModel))
+        await controller.login(req, res, next);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(returnMock);
+        
+      });
+    });
+
+    describe('Fail case', () => {
+      let req = mockRequest();
+      let res = mockResponse();
+      let next = mockNext();
+      beforeEach(() => {
+        req.body = {
+          username: "breno5g",
+          password: "@Teste01"
+        }
+        vitest.spyOn(UserService.prototype, 'login').mockImplementation(async () => {
+          throw new UsernameOrPasswordNotFoundError()
+        });
+      });
+  
+      afterAll(() => {
+        vitest.clearAllMocks()
+      });
+  
+      it('Username or password not found', async () => {
+          const controller = new UserController(new UserService({} as any))
+          await controller.login(req, res, next);
+          
+          expect(next).toHaveBeenCalledWith({
+            message: "Username or Password not found",
+            status: 400
           })
       });
     });
